3a3923140d52828a62b10a7ab629bf2fcd08024c
PiyushPb <piyushp0512@gmail.com>
2022-12-12 14:55:54 +0530

    updated website on 12 12 2022

--- work/js/portfolio.js
+++ work/js/portfolio.js
@@ -1,7 +1,7 @@
 paceOptions = {
     ajax: true,
     document: true,
-    eventLag: false
+    eventLag: false,
 };
 
 Pace.on('done', function() {
@@ -16,6 +16,13 @@ Pace.on('done', function() {
         right: '-100%',
         width: '0'
     }, 3000, $.bez([0.19, 1, 0.22, 1]));
+
+
+    TweenMax.to("nav", 3, {
+        delay: 1.4,
+        opacity: 1,
+        ease: Expo.easeInOut,
+    });
 
 
 
@@ -40,90 +47,178 @@ Pace.on('done', function() {
     }, 0.12);
 
 
-const ourWrok = gsap.timeline({
+    const ourWrok = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".container1",
+            start: "top center",
+
+        }
+    });
+
+    ourWrok.to(".overlay_", {
+        duration: 1.3,
+        width: "0px",
+        ease: Expo.easeInOut,
+
+    });
+
+
+    const image1 = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".image_container1",
+            start: "top center+=250px",
+        }
+    });
+
+    image1.from(".image_container1", {
+        duration: 1.3,
+        width: "0px",
+        ease: Expo.easeInOut,
+    });
+
+
+    const image2 = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".image_container2",
+            start: "top center+=150px",
+        }
+    });
+
+    image2.from(".image_container2 div", {
+        duration: 1.3,
+        paddingTop: "0%",
+        ease: Expo.easeInOut,
+    });
+
+
+    const ourWrok2 = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".container2",
+            start: "top center",
+        }
+    });
+
+    ourWrok2.to(".overlay__", {
+        duration: 1.3,
+        width: "0px",
+        ease: Expo.easeInOut,
+    });
+
+    const image3 = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".image_container3",
+            start: "top center",
+        }
+    });
+
+    image3.from(".image_container3 div", {
+        duration: 1.3,
+        paddingTop: "0%",
+        ease: Expo.easeInOut,
+    });
+
+    const colorPallet_heading = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".colorPallet_heading",
+            start: "top center",
+        }
+    });
+
+    colorPallet_heading.to(".overlay___", {
+        duration: 1.3,
+        width: "0px",
+        ease: Expo.easeInOut,
+    });
+
+
+    const image_container_color_pallet = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".image_container_color_pallet",
+            start: "top center+=140px",
+        }
+    });
+
+    image_container_color_pallet.from(".image_container_color_pallet div", {
+        duration: 1.3,
+        paddingTop: "0%",
+        ease: Expo.easeInOut,
+    });
+
+
+    const font_heading = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".font_heading",
+            start: "top center+=150px",
+        }
+    });
+
+    font_heading.to(".overlay____", {
+        duration: 1.3,
+        width: "0px",
+        ease: Expo.easeInOut,
+    });
+
+
+    const image_container_font = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".image_container_font",
+            start: "top center+=140px",
+        }
+    });
+
+    image_container_font.from(".image_container_font div", {
+        duration: 1.3,
+        paddingTop: "0%",
+        ease: Expo.easeInOut,
+    });
+
+
+});
+
+
+gsap.to('.menu_btn', {
     scrollTrigger: {
-        trigger: ".container1",
-        start: "top center",
+        trigger: ".ourWork_container",
+        start: "start center",
+        scrub: true,
+    },
+    color: "#000"
+})
 
-    }
+var nav = gsap.timeline({
+    paused: "true"
 });
-
-ourWrok.to(".overlay_", {
-    duration: 1.3,
-    width: "0px",
-    ease: Expo.easeInOut,
-
+nav.to(".menu", {
+    duration: 1,
+    x: "0%",
+    ease: Expo.easeInOut
 });
-
-
-const image1 = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".image_container1",
-        start: "top center+=250px",
-    }
+nav.fromTo(".li", {
+    y: "-100%",
+    opacity: 0
+}, {
+    duration: .5,
+    opacity: 1,
+    y: "0%",
+    stagger: 0.25
 });
+nav.fromTo(".social-li", {
+        y: "-50%",
+        opacity: 0
+    }, {
+        duration: 0.8,
+        opacity: 1,
+        stagger: 0.25,
+        ease: Expo.easeOut
+    },
+    "-=0.5");
 
-image1.from(".image_container1", {
-    duration: 1.3,
-    width: "0px",
-    ease: Expo.easeInOut,
-});
+function toggle() {
+    nav.play();
+}
 
+function togglec() {
+    nav.reverse();
+}
 
-const image2 = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".image_container2",
-        start: "top center+=150px",
-    }
-});
-
-image2.from(".image_container2 div", {
-    duration: 1.3,
-    paddingTop: "0%",
-    ease: Expo.easeInOut,
-});
-
-
-const ourWrok2 = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".container2",
-        start: "top center",
-    }
-});
-
-ourWrok2.to(".overlay__", {
-    duration: 1.3,
-    width: "0px",
-    ease: Expo.easeInOut,
-});
-
-const image3 = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".image_container3",
-        start: "top center",
-    }
-});
-
-image3.from(".image_container3 div", {
-    duration: 1.3,
-    paddingTop: "0%",
-    ease: Expo.easeInOut,
-});
-
-
-
-const colorPallet = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".brand_details_content_container",
-        start: "top center",
-    }
-});
-
-colorPallet.to(".overlay___", {
-    duration: 1.3,
-    width: "0px",
-    ease: Expo.easeInOut,
-});
-
-
-});
+AOS.init();
